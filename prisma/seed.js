4813cc1796ff82b3bf1f3aeba24ab0d9f0dd72de
Scalify Labs <hello@scalifylabs.com>
2026-01-02 19:31:04 +0530

    Add admin seed script

---
+++ prisma/seed.js
@@ -0,0 +1,36 @@
+const { PrismaClient } = require('@prisma/client');
+const bcrypt = require('bcryptjs');
+
+const prisma = new PrismaClient();
+
+async function main() {
+    const email = 'admin@scalifylabs.com';
+    const password = 'admin'; // Simple password for initial access
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.upsert({
+        where: { email },
+        update: {
+            password: hashedPassword,
+            role: "ADMIN"
+        },
+        create: {
+            email,
+            name: 'Admin User',
+            password: hashedPassword,
+            role: 'ADMIN',
+        },
+    });
+
+    console.log({ user });
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
